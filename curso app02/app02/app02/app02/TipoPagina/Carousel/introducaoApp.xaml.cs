eabd9585fb5f0d9b9534f69afb30f79f5174d6cc
CLEISSON GUIMARAES SANTOS <cleisson.santos@external.cnhind.com>
2019-04-05 21:55:49 -0300

    App02

---
+++ curso app02/app02/app02/app02/TipoPagina/Carousel/introducaoApp.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app02.TipoPagina.Carousel
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class introducaoApp : CarouselPage
+	{
+		public introducaoApp ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
